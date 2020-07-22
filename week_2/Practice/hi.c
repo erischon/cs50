3bf8efb98a411e285bc75e4533b34e534e6a11e9
Eri Schon <erischon@gmail.com>
2020-07-22 17:29:48 +0200

    w2 practice

---
+++ week_2/Practice/hi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c1 = 'H';
+    char c2 = 'I';
+    char c3 = '!';
+    printf("%c %c %c\n", c1, c2, c3);
+    printf("%i %i %i\n", (int) c1, (int) c2, (int) c3);
+    printf("%i %i %i\n", c1, c2, c3);
+    printf("======================");
+    int i1 = 72;
+    int i2 = 73;
+    int i3 = 33;
+    printf("\n%c %c %c\n", c1, c2, c3);
+}
